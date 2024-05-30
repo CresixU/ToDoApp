f63b51cbe7bf5cc153358872ff44372072db86dd
Krystian Wawruch <CresixU@gmail.com>
2024-05-30 15:45:22 +0200

    Dodanie testów api

--- todo-api/src/config/database.ts
+++ todo-api/src/config/database.ts
@@ -5,13 +5,13 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGO_URI;
+    const dbURI = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI;
 
     if(!dbURI)
         throw new Error("MongoDB connection string is not defined in .env file");
 
     await mongoose.connect(dbURI); 
-    console.log('MongoDB connected');
+    console.log(`MongoDB connected. Database: ${process.env.NODE_ENV}`);
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
